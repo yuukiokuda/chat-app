23e8c521922cec0cc8c65103e0ea56c4a9bd6adb
Yuuki Okuda <136579520+yuukiokuda@users.noreply.github.com>
2023-06-19 15:45:29 +0900

    Revert "フロント準備"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-  root to: "messages#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
